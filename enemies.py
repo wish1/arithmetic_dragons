a30b2c8e27080aed567a569ca42cb82207c6b43d
egorrozinskiy <egor.rozinskiy@mail.ru>
2016-11-17 17:47:36 +0300

    ы

--- enemies.py
+++ enemies.py
@@ -39,7 +39,32 @@ class GreenDragon(Dragon):
         self.set_answer(x + y)
         return self.__quest
 
-#FIXME здесь также должны быть описаны классы RedDragon и BlackDragon
+class RedDragon(Dragon):
+    def __init__(self):
+        self._health = 200
+        self._attack = 10
+        self._color = 'красный'
+
+    def question(self):
+        x = randint(1,100)
+        y = randint(1,100)
+        self.__quest = str(x) + '-' + str(y)
+        self.set_answer(x - y)
+        return self.__quest
+
+class BlackDragon(Dragon):
+    def __init__(self):
+        self._health = 200
+        self._attack = 10
+        self._color = 'черный'
+
+    def question(self):
+        x = randint(1,10)
+        y = randint(1,10)
+        self.__quest = str(x) + '*' + str(y)
+        self.set_answer(x * y)
+        return self.__quest
+
 # красный дракон учит вычитанию, а чёрный -- умножению.
 
 
